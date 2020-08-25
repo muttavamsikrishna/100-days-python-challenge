f63c5c48388e93abee696530c41e97a82b8ad4c0
muttavamsikrishna <66399320+muttavamsikrishna@users.noreply.github.com>
2020-08-25 17:25:15 +0530

    Add files via upload

---
+++ 8_nearest_prime.py
@@ -0,0 +1,18 @@
+import math
+def prime(n):
+    for i in range(2,int(math.sqrt(n))+1):
+        if n%i==0:
+            return False
+    else:
+        return True
+n=int(input())
+for i in range(n-1,1,-1):
+    if prime(i):
+        print(i)
+        break
+while(n>1):
+    if prime(n):
+        print(n)
+        break
+    n=n+1
+    
